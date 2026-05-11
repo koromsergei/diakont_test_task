fa224a658de59e22a4e91d2af1b5225bbe1afafb
Sergei <sv.korom@gmail.com>
2026-05-11 13:40:24 +0300

    m1 is done, test needs

--- M1/server.py
+++ M1/server.py
@@ -1,7 +1,7 @@
 import socket
+from diakont_test_task.common.config import HOST, PORT
 
-
-def recive_all(conn, length):
+def receive_all(conn, length):
     data = b""
     while len(data) < length:
         chunk = conn.recv(length - len(data))
@@ -12,10 +12,8 @@ def recive_all(conn, length):
     return data
 
 
-def run_server(on_respond=None, on_pack_id=None, on_closed=None):
-    # TODO: добавить задание порта и адреса в файл
-    HOST = "127.0.0.1"
-    PORT = 5000
+def run_server(on_respond=None, on_pack_id=None, on_closed=None, send_respond=None):
+
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
@@ -23,7 +21,7 @@ def run_server(on_respond=None, on_pack_id=None, on_closed=None):
 
     print(f"Server listening on {HOST}:{PORT}")
     while True:
-        conn = ''
+        conn = None
         try:
             conn, addr = server.accept()
             if on_respond is not None:
@@ -32,7 +30,7 @@ def run_server(on_respond=None, on_pack_id=None, on_closed=None):
             print(f"Connected: {addr}")
 
             data_length = conn.recv(1)[0]
-            data = recive_all(conn, data_length)
+            data = receive_all(conn, data_length)
             data_type = data[1]
             data_number = data[2:4]
             data_message = data[3:]
@@ -41,7 +39,8 @@ def run_server(on_respond=None, on_pack_id=None, on_closed=None):
             if data_message and on_pack_id is not None:
                 if data_message == b'0x0000':
                     on_pack_id(data_number)
-                    conn.sendall(recive_all)
+                    if send_respond():
+                        conn.sendall(receive_all)
                 if data_message == b'0xFFFF':
                     on_closed(True)
 
--- M1/widgets/main_panel.py
+++ M1/widgets/main_panel.py
@@ -1,9 +1,10 @@
-from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtCore import Qt, pyqtSignal, QTimer
 from PyQt5.QtWidgets import QCheckBox, QLabel, QApplication
 from diakont_test_task.common.M.widgets.common_panel import CommonPanel
+from diakont_test_task.common.config import TIME_TO_CLOSE
 import threading
-
 from server import run_server
+
 
 class MainPanel(CommonPanel):
     message_respond_received = pyqtSignal(bool)
@@ -12,13 +13,13 @@ class MainPanel(CommonPanel):
 
     def __init__(self):
         super().__init__() 
-
         self.chechbox = QCheckBox("respond")
-        
-        self.lable = QLabel("Pack_id")
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.close_all)
+        self.label = QLabel("Pack_id")
 
         self.layout.addWidget(self.chechbox, alignment=Qt.AlignRight)
-        self.layout.addWidget(self.lable, alignment=Qt.AlignRight)
+        self.layout.addWidget(self.label, alignment=Qt.AlignRight)
 
         # тут определяется функция, которая вызывается при срабатывании сигнала
         self.message_respond_received.connect(self.on_server_respond)
@@ -29,10 +30,24 @@ class MainPanel(CommonPanel):
             target=run_server, 
             args=(self.handle_server_respond,
                   self.handle_server_pack_id,
-                  self.handle_server_closed
+                  self.handle_server_closed,
+                  self.send_respond
                    ),
             daemon=True)
         self.server_thread.start()
+        self.chechbox.toggled.connect(self.on_checkbox_changed)
+
+    
+    def close_all(self):
+        QApplication.instance().quit()
+    
+    def send_respond(self):
+        return self.chechbox.isChecked()
+
+
+    def on_checkbox_changed(self, checked):
+        if not checked:
+            self.timer.start(TIME_TO_CLOSE)
 
 
     def handle_server_respond(self, flag):
@@ -54,7 +69,7 @@ class MainPanel(CommonPanel):
 
     def on_server_pack_id(self, value):
         if value is not None:
-            self.lable.setText(value)
+            self.label.setText(value)
     
 
     def on_server_closed(self, value):
